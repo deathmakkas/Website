65455e11c280f989b503ee76ab4ec468e4e85b52
deathmakkas <walterlukkarila123@gmail.com>
2020-08-29 15:29:42 -0700

    Ho

--- site.php
+++ site.php
@@ -2,13 +2,13 @@
 <html>
   <head>
     <meta charset = "utf-utf-8">
-    <title></title>
+    <title>Landing Page</title>
   </head>
   <body>
     <?php
-    echo "<h1 style='background-color:DodgerBlue;'>Hello World</h1>";
+    echo "<h1 style='background-color:DodgerBlue;'>Hello </h1>";
 
-      echo("Hello World")
+      echo("Hello")
 
      ?>
    </body>
