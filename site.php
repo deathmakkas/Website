ed4566e73965f4ce1bc75227062ea00b4dc28776
deathmakkas <walterlukkarila123@gmail.com>
2020-08-29 15:27:31 -0700

    Nice

---
+++ site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset = "utf-utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "<h1 style='background-color:DodgerBlue;'>Hello World</h1>";
+
+      echo("Hello World")
+
+     ?>
+   </body>
+   </html>
